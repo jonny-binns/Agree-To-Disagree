8f57ca6211377eac1d2f9efde7b1dfd2e901e9ef
Jonny Binns <jonny.binns1@gmail.com>
2020-10-12 16:16:59 +0100

    updated html and answers route

--- server.py
+++ server.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/answers/', methods=['POST', 'GET'])
-def answers_mode(name=None):
+def answers_mode(answer=None, prompt=None):
     if request.method == 'POST':
         print(request.form)
-        name = request.form['name']
-        return render_template('hello.html')
+        answer = request.form['answer']
+        #add answer to the databse
+        return redirect(url_for('answers_mode'))
     else:
-        return render_template('hello.html')
+        prompt = "this is an example prompt"
+        return render_template('ArgumentMode.html', debatePrompt=prompt)
     
 
 if __name__ == "__main__":
