306880427d51714626f3534d6f27b11485bf7766
Jonny Binns <jonny.binns1@gmail.com>
2020-10-30 12:29:54 +0000

    started adding bootstrap css, started menu route

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def menu():
+    return render_template('Menu.html')
+
 @app.route('/answers/', methods=['POST', 'GET'])
-def answers_mode(answer=None, prompt=None):
+def answers_mode(answer=None, prompt=None, proArgument=None, conArgument=None):
     if request.method == 'POST':
         print(request.form)
         answer = request.form['answer']
@@ -10,7 +14,9 @@ def answers_mode(answer=None, prompt=None):
         return redirect(url_for('answers_mode'))
     else:
         prompt = "this is an example prompt"
-        return render_template('ArgumentMode.html', debatePrompt=prompt)
+        proArgument = "this is an example argument in support of the prompt"
+        conArgument = "this is an example argument in opporsition of the prompt"
+        return render_template('ArgumentMode.html', prompt=prompt, proArgument=proArgument, conArgument=conArgument)
     
 
 if __name__ == "__main__":
