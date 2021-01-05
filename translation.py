3a1be4deecd15833f7b4ff6096d880c0ac89fed0
Jonny Binns <jonny.binns1@gmail.com>
2021-01-05 16:23:18 +0000

    menu page finished

--- translation.py
+++ translation.py
@@ -5,9 +5,11 @@ import json
 #method to read in form SADFace doc and take variables out
 def getArguments():
     sfdoc = sf.load_from_file("saves.json")
+    sf.set_doc(sfdoc)
     result, problems = sf.validation.verify(sf.get_document())
     if result:
         print(" ,".join(problems))
+        print(sf.prettyprint())
     else:
         print(sf.prettyprint())
     return None
